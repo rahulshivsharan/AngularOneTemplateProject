bf942accd180e79df22a21f89aa89ff0c4839e3b
rahulshivsharan <rahulshivsharan@gmail.com>
2017-10-09 18:25:36 +0530

    Added drop selection for state selection

--- src/app/main/controllers/modal.selectStates.controller.js
+++ src/app/main/controllers/modal.selectStates.controller.js
@@ -11,9 +11,16 @@
 		vm.init = init;
 		vm.ok = ok;
 		vm.cancel = cancel;
+		vm.selectState = selectState;
+		vm.findStatesByName = findStatesByName;
 
 		// public variables
 		vm.states = [];
+		vm.statesMap = {};
+		vm.searchState = "";
+
+		// private variables		
+		var copyOfStateMap = {};
 
 		//private methods
 
@@ -27,10 +34,39 @@
 		}
 
 		function init(){
-			console.log(dhisService.statesObject);
-			vm.states = _.keys(dhisService.statesObject);
-			console.log(vm.states);
-		}
+			//console.log(dhisService.statesObject);
+			var stateNameArray = _.keys(dhisService.statesObject);
+			
+			angular.forEach(stateNameArray,function(state,index){
+				vm.statesMap[state] = false;
+			});	
+
+			copyOfStateMap = angular.copy(vm.statesMap);
+		} // end of init
+
+		function selectState(stateName){
+			vm.statesMap[stateName] = !vm.statesMap[stateName];
+			copyOfStateMap[stateName] = !copyOfStateMap[stateName]; 			
+		}// end of selectState
+
+
+		function findStatesByName(){			
+			var map = {};
+			if(!angular.isDefined(vm.searchState) || vm.searchState === null || vm.searchState === ""){
+				angular.forEach(vm.statesMap,function(flag,stateName){
+					copyOfStateMap[stateName] = flag;
+				});
+				vm.statesMap = angular.copy(copyOfStateMap);
+			}else{
+				for(var prop in vm.statesMap){
+					if(prop.toLowerCase().includes(vm.searchState.toLowerCase())){
+						map[prop] = vm.statesMap[prop];	
+					}
+				}
+				vm.statesMap = map;				
+			}// end of else
+		}// end of findStatesByName
+
 	} // end of ModalSelectStatesController
 
 })();
